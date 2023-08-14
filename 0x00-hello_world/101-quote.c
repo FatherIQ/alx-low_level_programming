50c2e2a3b817f3bc40cf464aa716c1dcfd2a2164
FatherIQ <valadeife@gmail.com>
2023-08-14 16:57:12 -0700

    last digit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: error at 1
+ */
+int main
+{
+	write(n\"and that piece of art is useful" - Dora Korpar, 2015-10-19"\n);
+	return (1);
+}
