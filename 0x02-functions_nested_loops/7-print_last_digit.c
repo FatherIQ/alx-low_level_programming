7c913b21995ac2eb9dbfd3e6e57f09aa357a9e55
FatherIQ <valadeife@gmail.com>
2023-08-17 04:40:06 -0700

    last number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of a number
+ * @i: function parameter
+ * Return: a
+ */
+
+int print_last_digit(int i)
+{
+	int a;
+
+	a = i % 10;
+	if (i < 10)
+		a = -a;
+	_putchar(a + '0');
+	return (a);
+}
