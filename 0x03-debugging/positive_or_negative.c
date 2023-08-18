044e01c6c6f53c3f8147643058e210be599c4431
FatherIQ <valadeife@gmail.com>
2023-08-18 03:46:37 -0700

    positive and negative

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * positive_or_negative - function of positive and negative
+ *@i: parameter function
+ * Return: 0
+ */
+
 void positive_or_negative(int i)
 {
 	if (i > 0)
